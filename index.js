90b0a81d024108f1af8809068c99dcad09ee814c
Mihir160 <mihir.roy266@gmail.com>
2022-11-08 08:30:40 +0600

    databse connection and insert the data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 //middleware
@@ -12,6 +13,29 @@ app.get('/',(req, res) =>{
     res.send('b6a11-service-review-server is running')
 })
 
+
+
+
+const uri = `mongodb+srv://${process.env.DD_USER}:${process.env.DB_PASSWORD}@cluster0.ky0svg6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+     try{
+        const serviceCollection = client.db('ServiceReview').collection('services')
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+     }
+     finally{
+
+     }
+}
+
+run().catch(err => console.log(err))
 app.listen(port, () =>{
     console.log(`b6a11-service-review-server running on: ${port}`)
 })
