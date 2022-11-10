9c7ab66b3bdcd429583b0a53663318e8cb411e66
Mihir160 <mihir.roy266@gmail.com>
2022-11-10 11:56:06 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
@@ -19,6 +20,24 @@ app.get('/',(req, res) =>{
 const uri = `mongodb+srv://${process.env.DD_USER}:${process.env.DB_PASSWORD}@cluster0.ky0svg6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+     console.log(authHeader)
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        console.log(req.decoded)
+        next();
+    })
+}
 
 async function run(){
      try{
@@ -80,10 +99,17 @@ async function run(){
         
 
         //my review
-        app.get('/reviews', async(req, res) =>{
+        app.get('/reviews',verifyJWT, async(req, res) =>{
+            const decoded = req.decoded;
+            // console.log(decoded)
+            
+            if(decoded.email !== req.query.email){
+                res.status(403).send({message: 'unauthorized access'})
+            }
 
             let query = {}
-            if(req.query.email){
+            // console.log(req.query)
+            if (req.query.email) {
                 query = {
                     email: req.query.email
                 }
@@ -116,8 +142,18 @@ async function run(){
             };
             const result = await reviewCollection.updateOne(query, updateDoc, options);
             res.send(result)
-            console.log(result)
+            // console.log(result)
         })
+
+        //jwt token create
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            // console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ token })
+           
+        })
+
      }
      finally{
 
